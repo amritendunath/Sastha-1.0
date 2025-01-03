f100019e652184a281604b1cdf1343fbd86b9cb2
Amritendu Nath <amritendunath@yahoo.com>
2025-01-03 21:57:49 +0530

    Adding list info

---
+++ forward_base.py
@@ -0,0 +1,30 @@
+
+class ForwardChain:
+    def _init_(self):
+        self.clause_variable_list = ["High blood pressure and progression of disease",
+                                                     "Fluid Collection",
+                                                     "Inflammation",
+                                                     "Poor blood circulation",
+                                                     "Narrow blood vessels and chest pain",
+                                                     "Elevated Cholesterol",
+                                                     "Blood Clotting",
+                                                     "Increased Heart Rate",
+                                                     "Blockage in blood vessels"]
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
