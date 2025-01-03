d88cd100c6435f882bb1ed1fb841942911dd1cdd
Amritendu Nath <amritendunath@yahoo.com>
2025-01-03 21:59:12 +0530

    adding treatment_list

--- forward_base.py
+++ forward_base.py
@@ -10,21 +10,18 @@ class ForwardChain:
                                                      "Blood Clotting",
                                                      "Increased Heart Rate",
                                                      "Blockage in blood vessels"]
+        self.treatment_list=["ACE INHIBITOR / ARB BLOCKERS",
+                                               "DIURETICS / ALDOSTERONE ANTAGONISTS"
+                                               "CORTICOSTEROIDS",
+                                               "ACE INHIBITOR/ ANGIOTENSIN II RECEPTOR BLOCKERS",
+                                               "NITRATES",
+                                               "STATIN",
+                                               "VAGAL MANEUVER",
+                                               "CLOT PREVENTING DRUGS ( CLOPIDOGREL, TICAGRELOR)/ ASPIRIN",
+                                               "ANTI-ARRYTHMIC DRUGS",
+                                               "NITRATES/ OPEN HEART SURGERY"]                           
     
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
+        
 
 
 
