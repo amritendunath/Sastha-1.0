ae1144caee9e91afe1ebcef6adee3eafa0249b23
Amritendu Nath <amritendunath@yahoo.com>
2025-01-06 22:23:39 +0530

    class and func created

--- forward_rules.py
+++ forward_rules.py
@@ -1,2 +1,10 @@
 from queue import Queue
-import knowledge_base as kb
+import forward_base as kb
+
+class ForwardChain:
+    def __init__(self):
+        self.diagnosis_ailment = kb.ForwardChain.diagnosis_ailment
+        self.variable_initialized_list = kb.ForwardChain.variable_initialized_list
+        self.ailment_condition = kb.ForwardChain.ailment_condition
+        self.ailment_condition_treatment = kb.ForwardChain.ailment_condition_treatment
+        self.diagnosis_list = kb.ForwardChain.diagnosis_list
