a3bea1ba2a443ba4ff9d122d09af153698d1923a
Amritendu Nath <amritendunath@yahoo.com>
2025-01-08 09:25:22 +0530

    created class: ForwardRules and Func: initialize_ailment

--- forward_rules.py
+++ forward_rules.py
@@ -9,3 +9,23 @@ class ForwardChain:
         self.ailment_condition_treatment = kb.ForwardChain.ailment_condition_treatment
         self.variable_initialized_list = kb.ForwardChain.variable_initialized_list
 
+
+class ForwardRules:
+        def _init_(self, diagnosis):
+             self.diagnosis= diagnosis
+             self.variable_initialized_queue= Queue()
+             self.chain_forward = kb.ForwardChain
+        
+        def initialize_ailment(self, diagnosis_index):
+            for diagnosis_ailment_pair in self.chain_forward.diagnosis_ailment:
+                if diagnosis_index == diagnosis_ailment_pair[0]:
+                    ailment_index = diagnosis_ailment_pair[1]
+
+                for ailment_number in ailment_index:
+                    variable_initialized=  list(self.chain_forward.variable_initialized_list[ailment_number])
+                    variable_initialized[1]= self.chain_forward.ailment_condition[diagnosis_index][ailment_number]
+                    self.chain_forward.variable_initialized_list[ailment_number] = tuple(variable_initialized)
+                    self.variable_initialized_queue.put(self.chain_forward.variable_initialized_list[ailment_number])
+                            
+
+
