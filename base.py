2d5c1a0c1b3142633d1ee0ac904122336155dee7
Amritendu Nath <amritendunath@yahoo.com>
2025-01-09 20:14:35 +0530

    variable_list

--- base.py
+++ base.py
@@ -4,3 +4,10 @@ class Base:
              "Heart Failure", "Cardiomyopathy", "Angina", 
             "Coronary Artery Disease", "Tachycardia", "Ventricular Tachycardia"
         ]
+        self.variables_list = [
+            "Rapid/Irregular Heart Beats or Heart Palpitations", "Chest Pain",
+            "Persistent Shortness of Breath", "Fatigue", "Dizziness", "Lightheadedness", "Weakness", 
+            "Unexplained Sweating", "Fainting", "Weight Gain", "Edema", "Swollen Stomach", "Confusion", 
+            "Chest Tightness", "Vomiting", "Restlessness", "Heart Attack", "Nausea", "Tightness in Neck", 
+            "Cardiac Arrest", "Lung Congestion"
+        ]
