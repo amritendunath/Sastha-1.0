647ee3d1d8d6024b111693eb2c90875efb73d349
Amritendu Nath <amritendunath@yahoo.com>
2025-01-09 20:13:15 +0530

    self.conclusion

---
+++ base.py
@@ -0,0 +1,6 @@
+class Base:
+    def __init__(self):
+        self.conclusion = [
+             "Heart Failure", "Cardiomyopathy", "Angina", 
+            "Coronary Artery Disease", "Tachycardia", "Ventricular Tachycardia"
+        ]
