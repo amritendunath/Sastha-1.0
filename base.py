8528e2be1db962a022f6e0a9c637e08b94e44c83
Amritendu Nath <amritendunath@yahoo.com>
2025-01-10 11:15:23 +0530

    clauses for diseases, numerical value represents index in the variables list

--- base.py
+++ base.py
@@ -1,5 +1,6 @@
 class Base:
     def __init__(self):
+
         # all possible conclusions for our program
         self.conclusion = [
              "Heart Failure", "Cardiomyopathy", "Angina", 
@@ -14,6 +15,8 @@ class Base:
             "Chest Tightness", "Vomiting", "Restlessness", "Heart Attack", "Nausea", "Tightness in Neck", 
             "Cardiac Arrest", "Lung Congestion"
         ]
+
+        # list of all possible symptoms combination that leads to a diagnosis        
         self.symptoms_combination = [
             [0, 1, 6], [0, 12], [0, 20], [0, 10], [0, 11], [0, 18], [0, 19],
             [1, 2, 6], [1, 2, 7], [1, 2, 8], [1, 2, 9], [1, 10], [1, 11], [1, 13],
@@ -21,6 +24,14 @@ class Base:
             [1, 5, 6], [1, 5, 7], [1, 5, 9], [1, 12], [1, 6, 9], [1, 6, 7], [1, 6, 8],
             [1, 7], [1, 8, 9], [0, 16], [0, 17], [0, 7], [0, 8, 9], [1, 4, 7], [1, 4, 9]
         ]
+
+        # clauses for diseases, numerical value represents index in the variables list
+        self.heart_failure_clause = [0, 1, 3, 4, 6, 8, 9, 12, 20]
+        self.cardiomyopathy_clause = [0, 1, 2, 3, 5, 9, 10, 11]
+        self.angina_clause = [1, 2, 3, 4, 5, 6, 7, 13, 14, 15]
+        self.coronary_clause = [0, 2, 3, 6, 7, 16, 17]
+        self.tachycardia_clause = [0, 1, 2, 4, 5]
+        self.ventricular_tachycardia_clause = [0, 1, 2, 4, 5, 8, 18, 19]
 
         # pushing all clauses in the clause index list for easy retrieval later
         self.clause_index = [
