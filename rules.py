01a54487dc55d86d650dc66da83314587700ccc2
Amritendu Nath <amritendunath@yahoo.com>
2025-01-15 14:32:42 +0530

    Func:endProgram (for invoking the foward Chain)

--- rules.py
+++ rules.py
@@ -115,3 +115,11 @@ class Rules:
             self.final_diagnosis = self.current_knowledge.conclusions[self.diagnosis_index[(rule_num_to_process // 10) - 1]]
             print(f"\n\nThe patient might be suffering from {self.final_diagnosis}. Please perform appropriate tests to confirm and treat for the condition.\n\n")
             self.end_program()
+
+    def end_program(self):
+        treatment = fr.ForwardRules(self.get_diagnosis())
+        if self.get_diagnosis() == "":
+            treatment.end_program()
+        else:
+            treatment.initialize_forward_rule()        
+
