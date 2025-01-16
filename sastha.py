9a5b6bb1b83480dc5875845d70c8760d59e95ca6
Amritendu Nath <amritendunath@yahoo.com>
2025-01-16 09:14:29 +0530

    main()

--- sastha.py
+++ sastha.py
@@ -6,3 +6,11 @@ class Rules:
         # Placeholder for the logic to start the diagnosis iteration
         print("Starting diagnosis iteration...")
 
+def main():
+    print("Welcome to the Diagnosis-Treatment program for Heart Diseases!\n\n\n")
+    
+    heart_disease_diagnosis = fr.Rules()
+    heart_disease_diagnosis.start_iteration()
+
+if __name__ == "__main__":
+    main()
